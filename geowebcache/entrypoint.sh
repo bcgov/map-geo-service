ba9528aaadc8f017250abcbab5e29c138e62a969
ikethecoder <ikethecoder@copeconsulting.ca>
2023-08-19 15:45:36 -0700

    udp geowebcache config

--- geowebcache/entrypoint.sh
+++ geowebcache/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
- catalina.sh "run"
+envsubst </overrides/webapps/gwc/WEB-INF/geowebcache-core-context.xml > /usr/local/tomcat/webapps/gwc/WEB-INF/geowebcache-core-context.xml
+envsubst </overrides/webapps/gwc/WEB-INF/geowebcache-wmsservice-context.xml > /usr/local/tomcat/webapps/gwc/WEB-INF/geowebcache-wmsservice-context.xml
+
+catalina.sh "run"
